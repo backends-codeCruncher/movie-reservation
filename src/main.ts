17cefcb0521b1cbeba9c94f38319d2552d1d7582
Sergio Barreras <sergiobg.isc@gmail.com>
2024-09-21 14:55:24 -0700

    feat: use enviroment variables

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  const logger = new Logger('MainLogger');
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.setGlobalPrefix('api');
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT);
+
+  logger.log(`Server listening on port ${process.env.PORT}`);
 }
 bootstrap();
